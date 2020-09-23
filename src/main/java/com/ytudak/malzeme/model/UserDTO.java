6a4ab05b86bf6ed6272f69badd2521ee3c8918e8
ufukguler <ufukglr@yandex.com>
2020-09-23 15:52:42 +0300

    change password condition change

--- src/main/java/com/ytudak/malzeme/model/UserDTO.java
+++ src/main/java/com/ytudak/malzeme/model/UserDTO.java
@@ -9,4 +9,5 @@ import lombok.Data;
 @Data
 public class UserDTO {
     private String password;
+    private String oldPassword;
 }
--- src/main/java/com/ytudak/malzeme/services/UserService.java
+++ src/main/java/com/ytudak/malzeme/services/UserService.java
@@ -3,9 +3,11 @@ package com.ytudak.malzeme.services;
 import com.ytudak.malzeme.entity.User;
 import com.ytudak.malzeme.model.UserDTO;
 import com.ytudak.malzeme.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,20 +19,24 @@ import java.util.Optional;
 @Service
 public class UserService {
 
+    final private PasswordEncoder passwordEncoder;
     final private UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    @Autowired
+    public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
     }
 
     public Boolean changePassword(UserDTO userDTO) {
         Optional<User> currentUser = userRepository.findByUserName(profilePage());
         if (currentUser.isPresent()) {
-            String newPassword = new BCryptPasswordEncoder().encode(userDTO.getPassword());
-            currentUser.get().setPassword(newPassword);
-            userRepository.save(currentUser.get());
-            return true;
-
+            if (passwordEncoder.matches(userDTO.getOldPassword(), currentUser.get().getPassword())) {
+                String newPassword = new BCryptPasswordEncoder().encode(userDTO.getPassword());
+                currentUser.get().setPassword(newPassword);
+                userRepository.save(currentUser.get());
+                return true;
+            }
         }
         return false;
     }
@@ -38,7 +44,7 @@ public class UserService {
     // get current users username
     public String profilePage() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String username = null;
+        String username;
         if (principal instanceof UserDetails) {
             username = ((UserDetails) principal).getUsername();
         } else {
